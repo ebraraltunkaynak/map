6f087038fe228d3a5f8c1e6ac97ba9a8aac9415c
Ebrar <16155052@mersin.edu.tr>
2021-07-16 17:56:51 +0300

    map a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import Icon from 'leaflet/dist/images/marker-icon-2x.png';
+import Shadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: Icon,
+  shadowUrl: Shadow
+});
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[37.183582, 28.486397]} zoom={9} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright%22%3EOpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[37.183582, 28.486397]}>
+          <Popup>
+           Hello <br /> You' re here.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
